5bfc82a67ebddb958b63ff039a6f8f98ff2fe16f
Harsh Jain <66999104+Harshjain007@users.noreply.github.com>
2021-07-24 11:10:28 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,7 @@
+public class Array{
+     public static void main(String[] args) {
+      int arr1[] = {1,2,8,9,5};
+         for(int i:arr1){
+             System.out.println(i);
+         }
+     }
